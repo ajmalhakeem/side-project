8f343708d4c3b60d73bbb3c5fe1b28543d16c610
hkmjtech <ajmlhkm@gmail.com>
2022-07-07 20:41:51 +0800

    second-feature: used extraProp1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Background } from "../components/Background";
 import { Table } from "../components/Table";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <Table title={""} content={""} numberToDisplay={2}></Table>;
+  return (
+    <Table
+      title={""}
+      content={""}
+      numberToDisplay={2}
+      extraProp1="please don't remove"
+    ></Table>
+  );
 };
 
 export default Home;
