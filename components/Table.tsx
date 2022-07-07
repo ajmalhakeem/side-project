21bf565454d34b190b7109a9ba0db1c0440af838
hkmjtech <ajmlhkm@gmail.com>
2022-07-07 20:44:04 +0800

    dawdk

--- components/Table.tsx
+++ components/Table.tsx
@@ -18,6 +18,11 @@ type TableProps = {
   title: string;
   content: string;
   numberToDisplay: evenNumbers;
+  extraProp1?: string;
+  extraProp2?: string;
+  extraProp3?: string;
+  extraProp4?: string;
+  extraProp5?: string;
 };
 
 export const Table = ({
@@ -26,6 +31,7 @@ export const Table = ({
   numberToDisplay,
   ...rest
 }: TableProps) => {
+  const { extraProp1, extraProp2, extraProp3, extraProp4, extraProp5 } = rest;
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
@@ -41,7 +47,7 @@ export const Table = ({
             type="button"
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
           >
-            Export
+            {extraProp1}
           </button>
         </div>
       </div>
